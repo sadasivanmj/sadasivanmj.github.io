a712cbe6898dc21cc4df9b2e01d1adfcb934c0e8
Sadasivan M J <sadasivanmj2003@gmail.com>
2026-02-08 23:42:04 +0530

    as

--- desktop.js
+++ desktop.js
@@ -29,12 +29,3 @@ if (isTouchDevice) {
     openArticles();
   });
 }
-
-const ARTICLES_URL = new URL(
-  "articles/article.html",
-  window.location.href
-).href;
-
-function openArticles() {
-  window.location.href = ARTICLES_URL;
-}
